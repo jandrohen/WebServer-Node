4fcee37ebfab60cc5f704495e21b0b5cd16946f1
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-12-21 18:42:15 +0100

    (lesson 93 course node) Example create elements partials from the controller with Handlebars hbs library

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require('express')
-const app = express()
-const port = 8080
+const hbs = require('hbs');
 
+const app = express();
+const port = 8080;
 
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Servir contenido estático
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('Home',{
+    res.render('home',{
         nombre: 'Alejandro',
         titulo: 'Curso de Node'
     });
 })
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic',{
+        nombre: 'Alejandro',
+        titulo: 'Curso de Node'
+    });
 })
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements',{
+        nombre: 'Alejandro',
+        titulo: 'Curso de Node'
+    });
 })
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
